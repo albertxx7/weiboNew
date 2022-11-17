7cdf642de35f7bc190bb091f6e2f71525d00d46b
albertxx7 <a979845623@gmail.com>
2022-11-17 05:59:39 +0000

    fix Routes

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -7,14 +7,14 @@ use Illuminate\Http\Request;
 class StaticPagesController extends Controller
 {
 public function home(){
-    return view('static_Pages/home');
+    return view('static_pages/home');
 }
 
 public function  help(){
-    return view('static_Pages/help');
+    return view('static_pages/help');
 }
 
 public function  about(){
-    return view('static_Pages/about');
+    return view('static_pages/about');
 }
 }
