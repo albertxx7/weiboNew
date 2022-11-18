5cbb215e0ae445c96e359dcf077f05d50d926a9d
albertxx7 <a979845623@gmail.com>
2022-11-18 09:03:57 +0000

    設定路由器

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,10 +1,10 @@
 <footer class="footer">
-  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
-  <a href="https://github.com/albertxx7/weiboNew" target=_blank class="text-decoration-none">
-    以記錄療癒世界
-  </a>
+    <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+    <a href="https://github.com/albertxx7/weiboNew" target=_blank class="text-decoration-none">
+        以記錄療癒世界
+    </a>
 
-  <div class="float-end">
-    <a href="/about" class="text-decoration-none">關於</a>
-  </div>
+    <div class="float-end">
+        <a href="{{ route('about') }}" class="text-decoration-none">關於</a>
+    </div>
 </footer>
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="/">Weibo 微聊</a>
+        <a class="navbar-brand" href="{{ route('home') }}">Weibo 微聊</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
