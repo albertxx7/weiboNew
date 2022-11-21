c0ebc66b9a811db7de1850aa29c69fd93e18dc64
albertxx7 <a979845623@gmail.com>
2022-11-21 05:59:25 +0000

    Updated database configuration fix

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
-Route::resource('users', 'Userscontroller');
+Route::resource('users', 'UsersController');
